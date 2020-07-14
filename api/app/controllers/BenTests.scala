45b0ad1884bf435208d7f7457515d95a4aa17ae5
Ben Iofel <ben@flow.io>
2020-07-14 15:42:05 -0400

    add call

--- api/app/controllers/BenTests.scala
+++ api/app/controllers/BenTests.scala
@@ -1,18 +1,23 @@
 package controllers
 
 import akka.actor.ActorSystem
-import com.github.ghik.silencer.silent
 import io.flow.play.controllers.{FlowController, FlowControllerComponents}
 import javax.inject.{Inject, Singleton}
+import play.api.libs.ws.WSClient
 import play.api.mvc.ControllerComponents
 
 @Singleton
 class BenTests @Inject()(
   system: ActorSystem,
   val controllerComponents: ControllerComponents,
-  val flowControllerComponents: FlowControllerComponents
+  val flowControllerComponents: FlowControllerComponents,
+  ws: WSClient,
 ) extends FlowController with FlowAssertions {
 
-  @silent private[this] implicit val ec = system.dispatchers.lookup("ben-test-controller")
+  private[this] implicit val ec = system.dispatchers.lookup("ben-test-controller")
+
+  def call(url: String) = Action.async { _ =>
+    ws.url(url).get().map(_ => Ok)
+  }
 
 }
